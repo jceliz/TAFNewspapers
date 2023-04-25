e3cb385fa0bde955039afbb3ebb63407d5a79494
j.celiz <celiz_jennel@yahoo.com>
2023-04-26 03:26:06 +0800

    delete files

--- TAFNewspapers/src/main/java/com/newspapers/locators/HomePageLocators.java
+++
@@ -1,9 +0,0 @@
-package com.newspapers.locators;
-
-import org.openqa.selenium.By;
-
-public class HomePageLocators {
-    public final static By LBL_USERNAME = By.xpath("//a[@id=\"globalMemNav\"]/descendant::span");
-    public final static By BTN_CLIPPINGS = By.xpath("//span[text()=\"Clippings\"]");
-
-}
